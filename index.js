60d7bbc0cf690a921ec39691851c70f37c6af5c5
Othniel N <142967341+Othniel-N@users.noreply.github.com>
2024-02-13 23:08:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(cors()); // Enable CORS for all routes
+app.use(express.json()); // Add this line for JSON body parsing
+
+// Your existing routes
+app.get('/api/user', (req, res) => {
+  const result = {
+    users: 50
+  };
+  res.json(result);
+});
+
+app.get('/api/test', (req, res) => {
+  res.send("working");
+});
+
+
+const tasks = [
+  { id: 1, name: 'Tamil' },
+  { id: 2, name: 'English' },
+  { id: 3, name: 'Maths' },
+  { id: 4, name: 'Science' },
+  { id: 5, name: 'Social science' }
+];
+
+app.get('/tasks', (req, res) => {
+  res.json(tasks);
+});
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
